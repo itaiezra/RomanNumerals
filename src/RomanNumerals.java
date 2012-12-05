6e699f85e7c96e31d49b1281204e3f5290a48fc2
itaiezra <itai@ItaiMobile>
2012-12-05 16:13:02 +0200

    work for 4

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -5,8 +5,11 @@ public class RomanNumerals
 	public String toRoman(int nmber) 
 	{
 		String num = "";
+		if (nmber<4)
 		for(int i=0;i<nmber;i++)
 			num+='I';
+		else 
+			num = "IV";
 		return num;
 	}
 
