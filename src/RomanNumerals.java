ab4cfad9842e70c58ce31c3a0b363515e21468e9
itaiezra <itai@ItaiMobile>
2012-12-05 21:13:42 +0200

    Works till 89

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -5,8 +5,8 @@ public class RomanNumerals
 	public String toRoman(int nmber) 
 	{
 		String num = "";
-		int[] aArray = {10,9,5,4,1};
-		String[] rArray = {"X","IX","V","IV","I"}; 
+		int[] aArray = {50,40,10,9,5,4,1};
+		String[] rArray = {"L","XL","X","IX","V","IV","I"}; 
 		for(int i=0; i<aArray.length; i++)
 		{
 			while(nmber >= aArray[i])
@@ -19,23 +19,3 @@ public class RomanNumerals
 		return num;
 	}	
 }
-
-/*
- *  if(aNumber < 1 || aNumber > 3999){
-return "-1";
-}
- 
-int[] aArray = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-String[] rArray = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-String rNumber = "";
- 
-for(int i=0; i<aArray.length; i++){
-while(aNumber >= aArray[i]){
-rNumber += rArray[i];
-aNumber -= aArray[i];
-}
-}
- 
-return rNumber;
-}*/
-
