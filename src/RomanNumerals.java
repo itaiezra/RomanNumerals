525237ccff2786aae97229fc63dcc3b87885937f
itaiezra <itai@ItaiMobile>
2012-12-05 21:16:58 +0200

    Should work for numbers <3999

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -5,8 +5,8 @@ public class RomanNumerals
 	public String toRoman(int nmber) 
 	{
 		String num = "";
-		int[] aArray = {50,40,10,9,5,4,1};
-		String[] rArray = {"L","XL","X","IX","V","IV","I"}; 
+		int[] aArray = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+		String[] rArray = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"}; 
 		for(int i=0; i<aArray.length; i++)
 		{
 			while(nmber >= aArray[i])
